e50e5ea75bbe47847739a67b33227160baa8238a
Sander Mathijs van Veen <smvv@kompiler.org>
2016-11-24 17:12:30 +0100

    Initial commit for esreduce

---
+++ gulpfile.js
@@ -0,0 +1,56 @@
+'use strict';
+
+var gulp = require('gulp');
+var mocha = require('gulp-mocha');
+var eslint = require('gulp-eslint');
+
+var TEST = [ 'test/*.js' ];
+
+var LINT = [
+    'gulpfile.js',
+    'esreduce.js'
+];
+
+var ESLINT_OPTION = {
+    'rules': {
+        'quotes': 0,
+        'eqeqeq': 0,
+        'no-use-before-define': 0,
+        'dot-notation': 0,
+        'no-shadow': 0,
+        'no-unused-vars': [
+            2,
+            {
+                'vars': 'all',
+                'args': 'none'
+            }
+        ],
+        'no-multi-spaces': 0,
+        'new-cap': [
+            2,
+            {
+                'capIsNew': false
+            }
+        ]
+    },
+    'env': {
+        'node': true
+    }
+};
+
+gulp.task('test', function () {
+    return gulp.src(TEST)
+        .pipe(mocha({
+            reporter: 'spec',
+            timeout: 100000 // 100s
+        }));
+});
+
+gulp.task('lint', function () {
+    return gulp.src(LINT)
+        .pipe(eslint(ESLINT_OPTION))
+        .pipe(eslint.formatEach('stylish', process.stderr))
+        .pipe(eslint.failOnError());
+});
+
+gulp.task('default', [ 'lint', 'test' ]);
