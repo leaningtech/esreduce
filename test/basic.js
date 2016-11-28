e6efa84ff401f4b1451e1a081bbcfac7c0eedd16
Sander Mathijs van Veen <smvv@kompiler.org>
2016-11-28 11:15:56 +0100

    Add test for removing all code and unused object properties

--- test/basic.js
+++ test/basic.js
@@ -11,8 +11,8 @@ describe('basic', function () {
     });
 
     it('can preserve simple code while removing comments', () => {
-        var source = 'if (1)\n    var x = 42; // comment';
-        var expected = 'if (1)\n    var x = 42';
+        var source = 'if (1)\n\tvar x = 42; // comment';
+        var expected = 'if (1)\n\tvar x = 42';
         var actual = esreduce.run(source, (code, ast) => {
             return code.indexOf(expected) > -1;
         });
@@ -42,6 +42,23 @@ describe('basic', function () {
         var expected = 'while (1) {\n}';
         var actual = esreduce.run(source, (code, ast) => {
             return code.indexOf('while (1)') > -1;
+        });
+        expect(actual).to.be.equal(expected);
+    });
+
+    it('can remove all code', () => {
+        var source = 'var a=null;var b=[null];var c={d:b,o:0};'
+        var expected = '';
+        var actual = esreduce.run(source, () => true);
+        expect(actual).to.be.equal(expected);
+    });
+
+    it('can remove unused object properties', () => {
+        var source = 'var a = {b: 2, c: 3, d: 4}; a.b'
+        var expected = 'var a = {\n\tb: 2\n};\na.b';
+        var actual = esreduce.run(source, (source, ast) => {
+            try { return eval(source) == 2; }
+            catch(e) { return false; }
         });
         expect(actual).to.be.equal(expected);
     });
