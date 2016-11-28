7b560fa38dc1e17eb2360b41842e76527dc9015a
Sander Mathijs van Veen <smvv@kompiler.org>
2016-11-28 16:37:08 +0100

    Add test case that expects an empty function body

--- test/simplify.js
+++ test/simplify.js
@@ -8,8 +8,22 @@ describe('simplify', function () {
     it('can remove unrelated code and simplify', () => {
         var source = 'if (1) { var a=1; var b=3; var c = 3; a + b }'
         var expected = 'var a = 1;\nvar b = 3;\na + b';
-        var actual = esreduce.run(source, (source, ast) => {
-            try { return eval(source) == 4; }
+        var actual = esreduce.run(source, (code, ast) => {
+            try { return eval(code) == 4; }
+            catch(e) { return false; }
+        });
+        expect(actual).to.be.equal(expected);
+    });
+
+    it('can create an empty function', () => {
+        var source = 'function foo(a,b,c,d){' +
+                     ';for(var i=b;i<c;i++){;a[i]=d};return a;};';
+        var expected = 'function foo(a, b, c, d) {\n\treturn a\n}';
+        var actual = esreduce.run(source, (code, ast) => {
+            try {
+                return eval(code + ';foo([],0,0,0)').length == 0 &&
+                    code.indexOf('function foo') > -1;
+            }
             catch(e) { return false; }
         });
         expect(actual).to.be.equal(expected);
